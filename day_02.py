5ae201742aef7fb77950d107382b29dc7c8789ff
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-02 08:36:23 +0100

    initial commit

--- day_02.py
+++ day_02.py
@@ -3,15 +3,36 @@ from time import perf_counter as pfc
 
 def read_puzzle(file):
     with open(file) as f:
-        return f.read()
+        return f.read().split("\n")
 
 
 def solve(puzzle):
-    result = puzzle
+    a = {"A X": 4,
+         "A Y": 8,
+         "A Z": 3,
+         "B X": 1,
+         "B Y": 5,
+         "B Z": 9,
+         "C X": 7,
+         "C Y": 2,
+         "C Z": 6,
+         }
+    b = {"A X": 3,
+         "A Y": 4,
+         "A Z": 8,
+         "B X": 1,
+         "B Y": 5,
+         "B Z": 9,
+         "C X": 2,
+         "C Y": 6,
+         "C Z": 7,
+         }
+
+    result = sum([a[game] for game in puzzle]), sum([b[game] for game in puzzle])
     return result
 
 
 start = pfc()
-# print(read_puzzle('Tag_01.txt'))
-print(*solve(read_puzzle('day_02.txt')))
+# print(read_puzzle('day_02.txt'))
+print(solve(read_puzzle('day_02.txt')))
 print(pfc() - start)
