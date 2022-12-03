ac026f4eb5d029c2d0306ff0bbe33cb60ecf27dc
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-03 13:48:15 +0100

    dict clarification

--- day_02.py
+++ day_02.py
@@ -1,38 +1,45 @@
 from time import perf_counter as pfc
 
+WIN, DRAW, LOST = 6, 3, 0
+ROCK, PAPER, SCISSORS = 1, 2, 3
 
+# 0=draw, 1=first player wins, 2=second player wins
+#  \ 2. R P S
+# 1.
+#  R    0 2 1
+#  P    1 0 2
+#  S    2 1 0
 def read_puzzle(file):
     with open(file) as f:
         return f.read().split("\n")
 
 
 def solve(puzzle):
-    a = {"A X": 4,
-         "A Y": 8,
-         "A Z": 3,
-         "B X": 1,
-         "B Y": 5,
-         "B Z": 9,
-         "C X": 7,
-         "C Y": 2,
-         "C Z": 6,
-         }
-    b = {"A X": 3,
-         "A Y": 4,
-         "A Z": 8,
-         "B X": 1,
-         "B Y": 5,
-         "B Z": 9,
-         "C X": 2,
-         "C Y": 6,
-         "C Z": 7,
-         }
+    part1 = {"A X": ROCK + DRAW,
+             "A Y": PAPER + WIN,
+             "A Z": SCISSORS + LOST,
+             "B X": ROCK + LOST,
+             "B Y": PAPER + DRAW,
+             "B Z": SCISSORS + WIN,
+             "C X": ROCK + WIN,
+             "C Y": PAPER + LOST,
+             "C Z": SCISSORS + DRAW
+             }
+    part2 = {"A X": LOST + SCISSORS,
+             "A Y": DRAW + ROCK,
+             "A Z": WIN + PAPER,
+             "B X": LOST + ROCK,
+             "B Y": DRAW + PAPER,
+             "B Z": WIN + SCISSORS,
+             "C X": LOST + PAPER,
+             "C Y": DRAW + SCISSORS,
+             "C Z": WIN + ROCK
+             }
 
-    result = sum([a[game] for game in puzzle]), sum([b[game] for game in puzzle])
+    result = sum([part1[game] for game in puzzle]), sum([part2[game] for game in puzzle])
     return result
 
 
 start = pfc()
-# print(read_puzzle('day_02.txt'))
 print(solve(read_puzzle('day_02.txt')))
 print(pfc() - start)
