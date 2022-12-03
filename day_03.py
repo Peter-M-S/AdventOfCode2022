d83c5f440ebeb2b4a4819ca3b135c2346d68932b
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-03 14:03:14 +0100

    refractoring

--- day_03.py
+++ day_03.py
@@ -1,13 +1,16 @@
 from time import perf_counter as pfc
 
+LOW_START = ord("a") - 1
+UP_START = ord("A") - 1 - 26
 def prio(a: str) -> int:
-    return ord(a) - 96 if a.islower() else ord(a) - (64-26)
+    return ord(a) - LOW_START if a.islower() else ord(a) - UP_START
 
 
 def read_puzzle(file):
     with open(file) as f:
         puzzle = []
         for line in f.read().split("\n"):
+            line = [prio(a) for a in line]
             n = len(line)//2
             puzzle.append((line[:n], line[n:]))
         return puzzle
@@ -16,14 +19,14 @@ def read_puzzle(file):
 def solve(puzzle):
     part1 = []
     for compartment0, compartment1 in puzzle:
-        a = set([prio(a) for a in compartment0]) & set([prio(a) for a in compartment1])
+        a = set(compartment0) & set(compartment1)
         part1.append(a.pop())
 
     part2 = []
     for i in range(0, len(puzzle), 3):
-        group = set([prio(a) for a in puzzle[i][0] + puzzle[i][1]])
+        group = set(puzzle[i][0] + puzzle[i][1])
         for j in range(1, 3):
-             group &= set([prio(a) for a in puzzle[i+j][0] + puzzle[i+j][1]])
+             group &= set(puzzle[i+j][0] + puzzle[i+j][1])
         part2.append(group.pop())
 
     return sum(part1), sum(part2)
