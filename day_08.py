c7d67d0f5b17585a819cd9fc725e994d61339d02
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-08 19:40:07 +0100

    refractoring

--- day_08.py
+++ day_08.py
@@ -6,15 +6,15 @@ def read_puzzle(file):
         return f.read().split("\n")
 
 
-def is_visible(p, pT, r, c):
-    left = max(p[r][:c]) < p[r][c]
-    right = max(p[r][c + 1:]) < p[r][c]
-    top = max(pT[c][:r]) < p[r][c]
-    bottom = max(pT[c][r + 1:]) < p[r][c]
+def is_visible(r, c):
+    left = max(puzzle[r][:c]) < puzzle[r][c]
+    right = max(puzzle[r][c + 1:]) < puzzle[r][c]
+    top = max(puzzleT[c][:r]) < puzzle[r][c]
+    bottom = max(puzzleT[c][r + 1:]) < puzzle[r][c]
     return any([left, right, top, bottom])
 
 
-def scenic_score(puzzle, puzzleT, r, c):
+def scenic_score(r, c):
     bench_mark = int(puzzle[r][c])
     to_top = max_distance(puzzleT[c][:r][::-1], bench_mark)  # reversed list to look against running r
     to_left = max_distance(puzzle[r][:c][::-1], bench_mark)  # reversed list to look against running c
@@ -23,7 +23,7 @@ def scenic_score(puzzle, puzzleT, r, c):
     return to_left * to_right * to_top * to_bottom
 
 
-def max_distance(heights: list[str], bench_mark: int) -> int:
+def max_distance(heights, bench_mark: int) -> int:
     distance = 0
     while distance < len(heights):
         distance += 1
@@ -33,21 +33,15 @@ def max_distance(heights: list[str], bench_mark: int) -> int:
 
 
 def solve(puzzle):
-    puzzleT = list(zip(*puzzle))        # transpose 2D list to easier look-up row slices
-
-    part1 = 2 * (len(puzzle) - 1 + len(puzzle[0]) - 1)      # trees at the edges
-    for r in range(1, len(puzzle) - 1):                     # loop thru inside trees only
-        for c in range(1, len(puzzle[0]) - 1):
-            part1 += is_visible(puzzle, puzzleT, r, c)
-
-    part2 = []
-    for r in range(1, len(puzzle) - 1):                     # loop thru inside trees only
-        for c in range(1, len(puzzle[0]) - 1):
-            part2.append(scenic_score(puzzle, puzzleT, r, c))
-
-    return part1, max(part2)
-
+    ROWS, COLS = len(puzzle), len(puzzleT)
+    part1 = 2 * (ROWS - 1 + COLS - 1)  # trees at the edges are all visible   # loop through inside trees only
+    part1 += sum([is_visible(r, c) for r in range(1, ROWS - 1) for c in range(1, COLS - 1)])
+    part2 = max([scenic_score(r, c) for r in range(1, ROWS - 1) for c in range(1, COLS - 1)])
+    # todo optimize by looping through inside trees only once for both parts?
+    return part1, part2
 
 start = pfc()
-print(*solve(read_puzzle('day_08.txt')))
+puzzle = read_puzzle('day_08.txt')
+puzzleT = list(zip(*puzzle))        # transpose 2D list to easier look-up row slices
+print(*solve(puzzle))
 print(pfc() - start)
