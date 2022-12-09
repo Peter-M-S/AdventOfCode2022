8531771d283b343d4a40679904d9bb4e08d9edbb
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-09 17:26:32 +0100

    refactoring move

--- day_09.py
+++ day_09.py
@@ -7,19 +7,19 @@ def read_puzzle(file):
         return [tuple([line.split()[0], int(line.split()[1])]) for line in f.readlines()]
 
 
-def move(pos, RUDL):
-    r, c = pos
-    if RUDL == "R":
-        c += 1
-    elif RUDL == "U":
-        r -= 1
-    elif RUDL == "D":
-        r += 1
-    elif RUDL == "L":
-        c -= 1
-    else:
-        print("error instruction")
-    return r, c
+def up(r, c): return r - 1, c
+
+
+def down(r, c): return r + 1, c
+
+
+def left(r, c): return r, c - 1
+
+
+def right(r, c): return r, c + 1
+
+
+MOVES = {"R": right, "U": up, "D": down, "L": left}
 
 
 def move_tail(pos, pos_head):
@@ -37,20 +37,22 @@ def move_tail(pos, pos_head):
         return (r + 1, c) if dr > 0 else (r - 1, c)
 
     # moving diagonal
-    r = r+1 if dr > 0 else r-1
-    c = c+1 if dc > 0 else c-1
+    r = r + 1 if dr > 0 else r - 1
+    c = c + 1 if dc > 0 else c - 1
     return r, c
 
+
 def simulation(knots, puzzle):
-    pos_knots = [(0,0) for _ in range(knots)]
+    pos_knots = [(0, 0) for _ in range(knots)]
     tail_positions = {pos_knots[-1]}
     for RUDL, l in puzzle:
         for i in range(l):
-            pos_knots[0] = move(pos_knots[0], RUDL)
+            pos_knots[0] = MOVES[RUDL](*pos_knots[0])
             for k in range(1, knots):
-                pos_knots[k] = move_tail(pos_knots[k], pos_knots[k-1])
+                pos_knots[k] = move_tail(pos_knots[k], pos_knots[k - 1])
             tail_positions.add(pos_knots[-1])
     return len(tail_positions)
+
 
 def solve(puzzle):
     return simulation(2, puzzle), simulation(10, puzzle)
