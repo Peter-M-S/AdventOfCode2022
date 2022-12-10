d3267228ae9121ef928c17353a3282fb783c0a2e
Peter-M-S <peter.m.schrader@gmail.com>
2022-12-10 13:00:33 +0100

    inital commit

--- day_10.py
+++ day_10.py
@@ -1,49 +1,38 @@
-from time import perf_counter as pfc
 import os
+from time import perf_counter as pfc
+
 
 def read_puzzle(file):
-    with open(file) as f:
-        return f.read().split("\n")
+    with open(file) as f: return f.read().split("\n")
 
 
 def solve(puzzle):
+    index = part1 = adding_completed = 0
     cycle = x = 1
-    sprite_position = [x-1, x, x+1]
-    index = part1 = 0
-    is_adding = False
+    sprite_position = [x - 1, x, x + 1]
 
     CRT = [["." for cycle in range(1, 41)] for row in range(6)]
-    for r in CRT:
-        print(*r)
 
     while index < len(puzzle):
-        cmd = puzzle[index][:4]
-        if cmd == "noop":
-            index += 1
+        if puzzle[index][:4] == "noop": index += 1
         else:
-            if is_adding:
+            if adding_completed:
                 x += int(puzzle[index][5:])
-
-                is_adding = False
+                adding_completed = False
                 index += 1
-            else:
-                is_adding = True
+            else: adding_completed = True
 
-        col = (cycle - 1) % 40
-        row = (cycle - 1) // 40
+        row, col = (cycle - 1) // 40, (cycle - 1) % 40
         CRT[row][col] = "#" if col in sprite_position else "."
 
         sprite_position = [x - 1, x, x + 1]
         cycle += 1
 
+        if cycle in range(20, 221, 40): part1 += x * cycle
 
-        if cycle in range(20,221,40):
-            print(cycle, index, x, x*cycle)
-            part1 += cycle*x
+    for r in CRT: print(*r)
 
-    part2 = "EHZFZHCZ"
-    for r in CRT:
-        print(*r)
+    part2 = "EHZFZHCZ"  # manual reading form screen, no character recognition
 
     return part1, part2
 
