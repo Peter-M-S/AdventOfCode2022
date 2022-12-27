a38809ebc1c56de2a0b389785f8a84a0f600db97
peter <peter.m.schrader@gmail.com>
2022-12-27 18:28:50 +0100

    added main()

--- day_18.py
+++ day_18.py
@@ -1,6 +1,7 @@
+import os
 from collections import deque
 from time import perf_counter as pfc
-import os
+
 
 def read_puzzle(file):
     with open(file) as f:
@@ -11,11 +12,11 @@ def solve(data):
     cubes = set()
     for line in data:
         x, y, z = map(int, line.split(","))
-        cubes.add((x,y,z))
+        cubes.add((x, y, z))
 
     # sides are between coordinates
-    offsets = [(0, 0, .5), (0, .5, 0), (.5, 0, 0),(0, 0, -.5), (0, -.5, 0), (-.5, 0, 0)]
-    neighbors = [(0, 0, 1), (0, 1, 0), (1, 0, 0),(0, 0, -1), (0, -1, 0), (-1, 0, 0)]
+    offsets = [(0, 0, .5), (0, .5, 0), (.5, 0, 0), (0, 0, -.5), (0, -.5, 0), (-.5, 0, 0)]
+    neighbors = [(0, 0, 1), (0, 1, 0), (1, 0, 0), (0, 0, -1), (0, -1, 0), (-1, 0, 0)]
     mx = my = mz = float("inf")
     Mx = My = Mz = -float("inf")
 
@@ -23,46 +24,51 @@ def solve(data):
     for x, y, z in cubes:
         mx, my, mz = min(mx, x), min(my, y), min(mz, z)
         Mx, My, Mz = max(Mx, x), max(My, y), max(Mz, z)
-        for dx, dy, dz in offsets:   # loop through all sides of x,y,z
-            side  = (x+dx, y+dy, z+dz)
-            if side not in sides:   # add new side to dict
+        for dx, dy, dz in offsets:  # loop through all sides of x,y,z
+            side = (x + dx, y + dy, z + dz)
+            if side not in sides:  # add new side to dict
                 sides[side] = 0
-            sides[side] += 1    # increase for each occurrence (if == 2 then this side belongs to two cubes = not open)
+            sides[side] += 1  # increase for each occurrence (if == 2 then this side belongs to two cubes = not open)
     part1 = sum(s for s in sides.values() if s == 1)
 
     # Part 2
     # define box just greater then cubes
-    pos0 = (mx-1, my-1, mz-1)
-    pos1 = (Mx+1, My+1, Mz+1)
+    pos0 = (mx - 1, my - 1, mz - 1)
+    pos1 = (Mx + 1, My + 1, Mz + 1)
 
-    outside = {pos0}    # collect all grid points outside of cubes
-    q = deque([pos0])   # queue for breadth first search
+    outside = {pos0}  # collect all grid points outside of cubes
+    q = deque([pos0])  # queue for breadth first search
 
     while q:
         x, y, z = q.popleft()
 
         for dx, dy, dz in neighbors:
-            nx, ny, nz = k  = (x+dx, y+dy, z+dz)
-            if not(pos0[0] <= nx <= pos1[0] and pos0[1] <= ny <= pos1[1] and pos0[2] <= nz <= pos1[2]):
+            nx, ny, nz = k = (x + dx, y + dy, z + dz)
+            if not (pos0[0] <= nx <= pos1[0] and pos0[1] <= ny <= pos1[1] and pos0[2] <= nz <= pos1[2]):
                 # out of box
                 continue
             if k in cubes or k in outside:
                 # position is already visited or blocked by cube
                 continue
             outside.add(k)
-            q.append(k)     # keep as candidate for next level neighbors
+            q.append(k)  # keep as candidate for next level neighbors
 
-    outside_sides = set()   # get all sides of outside grid points
-    for  x, y, z in outside:
+    outside_sides = set()  # get all sides of outside grid points
+    for x, y, z in outside:
         for dx, dy, dz in offsets:
-            outside_sides.add((x+dx, y+dy, z+dz))
+            outside_sides.add((x + dx, y + dy, z + dz))
 
     part2 = len(outside_sides & set(sides))  # intersection of all outside sides and cube sides
 
     return part1, part2
 
 
-file_name = os.path.basename(__file__)[:-3] + ".txt"
-start = pfc()
-print(*solve(read_puzzle(file_name)))
-print(pfc() - start)
+def main():
+    file_name = os.path.basename(__file__)[:-3] + ".txt"
+    start = pfc()
+    print(*solve(read_puzzle(file_name)))
+    print(pfc() - start)
+
+
+if __name__ == '__main__':
+    main()
